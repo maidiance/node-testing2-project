f7553630f54530e7154bc7321c81a433330a0a49
Winifred Lu <winnie.lu@comcast.net>
2022-02-24 15:11:23 -0500

    happy path tests done

--- api/server.test.js
+++ api/server.test.js
@@ -45,4 +45,38 @@ describe('test server endpoints', () => {
         result = await request(server).get('/flavors');
         expect(result.body).toHaveLength(1);
     });
+
+    test('[GET] /flavors/:id', async() => {
+        let result = await Flavors.insert({ name: 'popcorn' });
+        result = await request(server).get('/flavors/' + result.id);
+        expect(result.body.name).toBe('popcorn');
+    });
+
+    test('[POST] /flavors', async()=> {
+        let result = await request(server)
+            .post('/flavors')
+            .send({ name: 'bacon' });
+        expect(result.status).toBe(201);
+        result = await Flavors.getById(1);
+        expect(result.name).toBe('bacon');
+    });
+
+    test('[DELETE] /flavors/:id', async() => {
+        let {id} = await Flavors.insert({ name: 'caramel' });
+        let result = await request(server).delete('/flavors/' + id);
+        expect(result.status).toEqual(200);
+        expect(result.body).toEqual({ name: 'caramel', id: 1 });
+        const flavors = await db('flavors');
+        expect(flavors).toHaveLength(0);
+    });
+
+    test('[PUT] /flavors/:id', async() => {
+        let {id} = await Flavors.insert({ name: 'grape' });
+        let result = await request(server)
+            .put('/flavors/' + id)
+            .send({ name: 'blueberry' });
+        expect(result.body).toEqual({name: 'blueberry', id: 1});
+        let flavor = await Flavors.getById(id);
+        expect(flavor).toEqual({name: 'blueberry', id: 1});
+    })
 });
