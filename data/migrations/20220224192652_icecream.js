e322f2a93bc35c72e59b497db06c313286f92aef
Winifred Lu <winnie.lu@comcast.net>
2022-02-24 14:43:38 -0500

    migration and seed files done

--- data/migrations/20220224192652_icecream.js
+++ data/migrations/20220224192652_icecream.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("flavors", tbl => {
+        tbl.increments();
+    
+        tbl.string("name", 255).unique().notNullable();
+      });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists("flavors");
 };
